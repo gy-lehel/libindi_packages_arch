d2be650904d143d5936527d755646ba72822f3c8
Lehel Gyuro <lehel@freemail.hu>
2025-12-05 13:50:33 +0100

    2.1.7

--- update-version.sh
+++ update-version.sh
@@ -42,12 +42,12 @@ if [ ! -f "v${NEWVERSION}.tar.gz" ]; then
 fi
 NEWHASH="$(sha256sum "v${NEWVERSION}.tar.gz" | cut -f1 -d' ')"
 
-for firmware in $(ls firmware | grep -v -f firmware/ignore); do
+for firmware in $(ls firmware | grep -v -f firmware/ignore | grep -v -f "firmware/ignore.${architecture}"); do
   echo "Updating [${firmware}] to [${NEWVERSION}]"
   update_version "firmware/${firmware}/PKGBUILD" "${NEWVERSION}" "${NEWHASH}"
 done
 
-for driver in $(ls drivers | grep -v -f drivers/ignore); do
+for driver in $(ls drivers | grep -v -f drivers/ignore | grep -v -f "drivers/ignore.${architecture}"); do
   echo "Updating [${driver}] to [${NEWVERSION}]"
   update_version "drivers/${driver}/PKGBUILD" "${NEWVERSION}" "${NEWHASH}"
 done
