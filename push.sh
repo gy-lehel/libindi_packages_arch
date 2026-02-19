ed8dbeb2730743e79f67a1c7edec823d1f216811
Lehel Gyuro <lehel@freemail.hu>
2026-02-19 10:32:58 +0100

    Update pusher to use an externally defined version

--- push.sh
+++ push.sh
@@ -9,10 +9,15 @@ source ./common.sh
 ################################################################################
 function push() {
   pushd $1
+
+  if ! grep -q "pkgver = ${new_version}" .SRCINFO
+  then
+    makepkg --printsrcinfo >.SRCINFO
+  fi
+
   if [[ $(git status --porcelain --untracked-files=no | wc -l) -gt 0 ]]
   then
     local version="$(grep "pkgver=" "PKGBUILD" | cut -f2 -d'=')"
-    makepkg --printsrcinfo >.SRCINFO
     git add -u
     git commit -m "v${version}"
   fi
@@ -25,5 +30,6 @@ function push() {
 }
 
 ################################################################################
+readonly VERSION="${1}"
 foreach_dir push firmware
 foreach_dir push drivers 
